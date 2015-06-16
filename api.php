735679c8745336fea8c69509bf9d8fa6f7d04610
Guernouille <froggy25@hotmail.fr>
2015-06-16 19:00:01 +0200

    pdo, fixes
    Je n'ai pas testé mais ça devrait marcher. Je te fais confiance pour corriger les parse errors s'il manque un point-virgule :P

--- api.php
+++ api.php
@@ -46,16 +46,13 @@
 		) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
  	*/
 	
+	require_once("config.php");
+	require_once("pdo.php");
 	require_once("rest/Rest.inc.php");
 	
 	class API extends REST {
 	
 		public $data = "";
-		
-		const DB_SERVER = "app.mysql.db";
-		const DB_USER = "app";
-		const DB_PASSWORD = "root";
-		const DB = "app";
 		
 		const MAX_EVENT_REQUEST = 101;
 		const MAX_MEDIA_REQUEST = 21;
@@ -72,9 +69,7 @@
 		 *  Database connection 
 		*/
 		private function dbConnect(){
-			$this->db = mysql_connect(self::DB_SERVER,self::DB_USER,self::DB_PASSWORD);
-			if($this->db)
-				mysql_select_db(self::DB,$this->db);
+			$this->db = PDO2::getInstance();
 		}
 		
 		/*
@@ -84,10 +79,9 @@
 		 */
 		public function processApi(){
 			$func = strtolower(trim(str_replace("/", "", $_REQUEST['rquest'])));
-			if((int)method_exists($this, $func) > 0)
-				$this->$func();
-			else
-				$this->response('', 404);				// If the method not exist with in this class, response would be "Page not found".
+			if(method_exists($this, $func)) $this->$func();
+			// Code 404 - Not Found
+			else $this->response('', 404);
 		}
 		
 		/* modify comment section using method request POST or GET */
@@ -95,51 +89,74 @@
 		private function comments() {
 			if($this->get_request_method() == "POST"){
 				// it's post method --> we will add a comment
-				$event = $this->_request['event'];
-				$content = $this->_request['content'];
-				$isReply = $this->_request['isReply'];
 				$pseudo = $this->_request['pseudo'];
+				$content = $this->_request['content'];
+				$event = $this->_request['event'];
+				$isReply = $this->_request['isReply'];
 				$replyTo = $this->_request['replyTo'];
 				
 				// Input validations
 				if(!empty($event) and !empty($content) and !empty($isReply) and !empty($pseudo) and !empty($replyTo)) {
-					$sql = mysql_query("INSERT INTO `comments` (`comment_id`, `comment_pseudo`, `comment_content`, `comment_event`, `comment_isReply`, `comment_replyTo`, `comment_date`)
-						VALUES ('', '$pseudo', '$content', '$event', '$isReply', '$replyTo', NOW())", $this->db);
-					if($sql){
+					$pre_requete = "INSERT INTO `comments` (`comment_id`, `comment_pseudo`, `comment_content`, `comment_event`, `comment_isReply`, `comment_replyTo`, `comment_date`)
+									VALUES ('',
+											:pseudo,
+											:content,
+											:event,
+											:isReply,
+											:replyTo,
+											NOW())";
+					$requete = $pdo->prepare($pre_requete);
+					$requete->bindValue(':pseudo', $pseudo);
+					$requete->bindValue(':content', $content);
+					$requete->bindValue(':event', $event);
+					$requete->bindValue(':isReply', $isReply);
+					$requete->bindValue(':replyTo', $replyTo);
+					
+					if($requete->execute()) {
+						// Code 200 - OK
 						$result = array('status' => "Success", "msg" => "Comment has been added");
-						// If success everythig is good send header as "OK" and user details
 						$this->response($this->json($result), 200);
 					}
-					$this->response('', 204);	// If no records "No Content" status
+					else{
+						// Code 204 - No content
+						$this->response('', 204);
+					}
 				}
-				// If invalid inputs "Bad Request" status message and reason
+				// Code 400 - Bad Request
 				$error = array('status' => "Failed", "msg" => "Invalid input");
 				$this->response($this->json($error), 400);
 				
-			} else if($this->get_request_method() == "GET"){
+			}
+			else if($this->get_request_method() == "GET"){
 				// it's get method --> we should take some comments and return them
-				$count = (int)$this->_request['count'];
+				if(ctype_digit(strval($this->_request['count']))) $count = strval($this->_request['count']);
+				else $count = 0;
+				
 				if($count > 0 /*and $count < MAX_COMMENT_REQUEST*/){
-					$sql = "SELECT COUNT(*) as nb FROM comments";
-					$req = mysql_query($sql) or die('Erreur SQL !'.$sql.' --- '.mysql_error());
-					$row = mysql_fetch_assoc($req);
-					$nombre = $row['nb'];
-					if($count > $nombre)
-						$count = $nombre;
-					mysql_query("SELECT * FROM comments ORDER BY comment_id DESC LIMIT $count");
-					if(mysql_num_rows($sql) > 0){
-						$result = array();
-						while($rlt = mysql_fetch_array($sql,MYSQL_ASSOC)) {
-							$result[] = $rlt;
-						}
-						// If success everythig is good send header as "OK" and return list of users in JSON format
-						$this->response($this->json($result), 200);
+					$nombre = $pdo->query("SELECT COUNT(*) as nb FROM comments")->fetchColumn();
+					if($count > $nombre) $count = $nombre;
+					
+					$pre_requete = "SELECT *
+									FROM comments
+									ORDER BY comment_id
+									DESC LIMIT :count";
+					$requete = $pdo->prepare($pre_requete);
+					$requete->bindValue(':count', $count);
+					$requete->execute();
+					$result = array();
+					
+					while($rlt = $requete->fetch(PDO::FETCH_BOTH)){
+						$result[] = $rlt;
 					}
-					$this->response('', 204);	// If no records "No Content" status
-				} else
-					$this->response('', 204);	// If no records "No Content" status
-			} else {
-				// we don't have functions for others request method, so we throw an error
+					// Code 200 - OK
+					if(!empty($result)) $this->response($this->json($result), 200);
+					// Code 204 - No content
+					else $this->response('', 204);
+				}
+				else $this->response('', 204);
+			}
+			else {
+				// Code 406 - Not Acceptable
 				$this->response('', 406);
 			}
 		}
@@ -151,30 +168,33 @@
 		 */
 		
 		private function medias() {
+			// Code 406 - Not Acceptable si la requête n'est pas en GET
 			if($this->get_request_method() != "GET") {
 				$this->response('', 406);
 			}
-			// now, shit's about to GET real
-			$count = (int)$this->_request['count'];
+			if(ctype_digit(strval($this->_request['count']))) $count = strval($this->_request['count']);
+			else $count = 0;
 			if($count > 0 and $count < MAX_MEDIA_REQUEST){
-				$sql = "SELECT COUNT(*) as nb FROM medias";
-				$req = mysql_query($sql) or die('Erreur SQL !'.$sql.'<br>'.mysql_error());
-				$row = mysql_fetch_assoc($req);
-				$nombre = $row['nb'];
-				if($count > $nombre)
-					$count = $nombre;
-				mysql_query("SELECT * FROM medias ORDER BY media_id DESC LIMIT $count");
-				if(mysql_num_rows($sql) > 0){
-					$result = array();
-					while($rlt = mysql_fetch_array($sql,MYSQL_ASSOC)){
-						$result[] = $rlt;
-					}
-					// If success everythig is good send header as "OK" and return list of users in JSON format
-					$this->response($this->json($result), 200);
+				$nombre = $pdo->query("SELECT COUNT(*) as nb FROM medias")->fetchColumn();
+				if($count > $nombre) $count = $nombre;
+				$pre_requete = "SELECT *
+								FROM medias
+								ORDER BY media_id
+								DESC LIMIT :count";
+				$requete = $pdo->prepare($pre_requete);
+				$requete->bindValue(':count', $count);
+				$requete->execute();
+				$result = array();
+				
+				while($rlt = $requete->fetch(PDO::FETCH_BOTH)){
+					$result[] = $rlt;
 				}
-				$this->response('', 204);	// If no records "No Content" status
-			} else
-				$this->response('', 204);	// If no records "No Content" status
+				// Code 200 - OK
+				if(!empty($result)) $this->response($this->json($result), 200);
+				// Code 204 - No content
+				else $this->response('', 204);
+			}
+			else $this->response('', 204);
 		}
 		
 		/*
@@ -184,30 +204,33 @@
 		 */
 		
 		private function events() {
+			// Code 406 - Not Acceptable si la requête n'est pas en GET
 			if($this->get_request_method() != "GET") {
 				$this->response('', 406);
 			}
-			// now, shit's about to GET real
-			$count = (int)$this->_request['count'];
+			if(ctype_digit(strval($this->_request['count']))) $count = strval($this->_request['count']);
+			else $count = 0;
 			if($count > 0 and $count < MAX_EVENT_REQUEST){
-				$sql = "SELECT COUNT(*) as nb FROM events";
-				$req = mysql_query($sql) or die('Erreur SQL !'.$sql.'<br>'.mysql_error());
-				$row = mysql_fetch_assoc($req);
-				$nombre = $row['nb'];
-				if($count > $nombre)
-					$count = $nombre;
-				mysql_query("SELECT * FROM events ORDER BY event_id DESC LIMIT $count");
-				if(mysql_num_rows($sql) > 0){
-					$result = array();
-					while($rlt = mysql_fetch_array($sql, MYSQL_ASSOC)){
-						$result[] = $rlt;
-					}
-					// If success everythig is good send header as "OK" and return list of users in JSON format
-					$this->response($this->json($result), 200);
+				$nombre = $pdo->query("SELECT COUNT(*) as nb FROM medias")->fetchColumn();
+				if($count > $nombre) $count = $nombre;
+				$pre_requete = "SELECT *
+								FROM events
+								ORDER BY media_id
+								DESC LIMIT :count";
+				$requete = $pdo->prepare($pre_requete);
+				$requete->bindValue(':count', $count);
+				$requete->execute();
+				$result = array();
+				
+				while($rlt = $requete->fetch(PDO::FETCH_BOTH)){
+					$result[] = $rlt;
 				}
-				$this->response('', 204);	// If no records "No Content" status
-			} else
-				$this->response('', 204);	// If no records "No Content" status
+				// Code 200 - OK
+				if(!empty($result)) $this->response($this->json($result), 200);
+				// Code 204 - No content
+				else $this->response('', 204);
+			}
+			else $this->response('', 204);	// If no records "No Content" status
 		}
 		
 		/* 
@@ -218,7 +241,7 @@
 		 */
 		
 		private function login(){
-			// Cross validation if the request method is POST else it will return "Not Acceptable" status
+			// Code 406 - Not Acceptable si la requête n'est pas en POST
 			if($this->get_request_method() != "POST"){
 				$this->response('',406);
 			}
@@ -226,54 +249,85 @@
 			$email = $this->_request['email'];		
 			$password = $this->_request['pwd'];
 			
-			// Input validations
 			if(!empty($email) and !empty($password)){
 				if(filter_var($email, FILTER_VALIDATE_EMAIL)){
-					$sql = mysql_query("SELECT user_id, user_fullname, user_email FROM users WHERE user_email = '$email' AND user_password = '".md5($password)."' LIMIT 1", $this->db);
-					if(mysql_num_rows($sql) > 0){
-						$result = mysql_fetch_array($sql,MYSQL_ASSOC);
-						
-						// If success everythig is good send header as "OK" and user details
-						$this->response($this->json($result), 200);
-					}
-					$this->response('', 204);	// If no records "No Content" status
+					$secure_password = sha1(SALT_1.$password.SALT_2)
+					$pre_requete = "SELECT	user_id,
+											user_fullname,
+											user_email
+									FROM	users
+									WHERE	user_email = :email
+									AND		user_password = :user_password
+									LIMIT	1";
+					$requete = $pdo->prepare($pre_requete);
+					$requete->bindValue(':email', $email);
+					$requete->bindValue(':user_password', $secure_password);
+					$requete->execute();
+					$result = "";
+					$result = $requete->fetch(PDO::FETCH_BOTH);
+					
+					// Code 200 - OK
+					if(!empty($result)) $this->response($this->json($result), 200);
+					// Code 204 - No content
+					else $this->response('', 204);
 				}
 			}
-			
-			// If invalid inputs "Bad Request" status message and reason
+			// Code 400 - Bad Request
 			$error = array('status' => "Failed", "msg" => "Invalid Email address or Password");
 			$this->response($this->json($error), 400);
 		}
 		
 		private function users(){	
-			// Cross validation if the request method is GET else it will return "Not Acceptable" status
+			// Code 406 - Not Acceptable si la requête n'est pas en GET
 			if($this->get_request_method() != "GET"){
 				$this->response('',406);
 			}
-			$sql = mysql_query("SELECT user_id, user_fullname, user_email FROM users WHERE user_status = 1", $this->db);
-			if(mysql_num_rows($sql) > 0){
-				$result = array();
-				while($rlt = mysql_fetch_array($sql,MYSQL_ASSOC)){
-					$result[] = $rlt;
-				}
-				// If success everythig is good send header as "OK" and return list of users in JSON format
-				$this->response($this->json($result), 200);
+			$pre_requete = "SELECT	user_id,
+									user_fullname,
+									user_email
+							FROM	users
+							WHERE	user_status = 1";
+			$requete = $pdo->prepare($pre_requete);
+			while($rlt = $requete->fetch(PDO::FETCH_BOTH)){
+				$result[] = $rlt;
 			}
-			$this->response('',204);	// If no records "No Content" status
+			// Code 200 - OK
+			if(!empty($result)) $this->response($this->json($result), 200);
+			// Code 204 - No content
+			else $this->response('', 204);
 		}
 		
 		private function deleteUser(){
-			// Cross validation if the request method is DELETE else it will return "Not Acceptable" status
+			// Code 406 - Not Acceptable si la requête n'est pas en DELETE
 			if($this->get_request_method() != "DELETE"){
 				$this->response('',406);
 			}
-			$id = (int)$this->_request['id'];
-			if($id > 0){
-				mysql_query("DELETE FROM users WHERE user_id = $id");
-				$success = array('status' => "Success", "msg" => "Successfully one record deleted.");
-				$this->response($this->json($success),200);
-			}else
-				$this->response('',204);	// If no records "No Content" status
+			
+			if($this->_request['pwd'] == DELETE_PASSWORD){
+				if(ctype_digit(strval($this->_request['id']))) $id = strval($this->_request['id']);
+				else $id = 0;
+				if($id > 0){
+					$pre_requete = "DELETE FROM users
+									WHERE user_id = :user_id";
+					$requete = $pdo->prepare($pre_requete);
+					$requete->bindValue(':user_id', $id);
+					if($requete->execute()){
+						// Code 200 - OK
+						$success = array('status' => "Success", "msg" => "Successfully one record deleted.");
+						$this->response($this->json($success),200);
+					}
+					else{
+						// Code 204 - No content
+						$success = array('status' => "Failed", "msg" => "User with this id not found.");
+						$this->response('',204);
+					}
+				}
+			}
+			else{
+				// Code 403 - Forbidden
+				$error = array('status' => "Failed", "msg" => "No you don't!");
+				$this->response($this->json($error), 403);
+			}
 		}
 		
 		/*
